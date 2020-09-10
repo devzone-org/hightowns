347c53f735a4494e8bf80d5c923105f030c81ede
Muhammad Talha <talha8018@gmail.com>
2020-09-10 15:52:15 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <style>
+        body {
+            background-color: #757575;
+            padding: 15px;
+        }
+
+        .margin-bottom-50 {
+            margin-bottom: 100px;
+
+        }
+        </style>
+        <title>Hightowns Contracting and Trading Est</title>
+    </head>
+
+    <body>
+
+
+        <div class="card">
+            <div class="card-body ">
+                <div class="container pt-5">
+                    <div class="row justify-content-center">
+                        <div class="col-12">
+                            <img class=" rounded  d-block" src="/images/images.jpg"
+                                alt="Hightowns Contracting and Trading Est">
+                            <h5 class="card-title margin-bottom-50">Hightowns Contracting and Trading Est</h5>
+                        </div>
+                        <div class="col-8 ">
+                            <h3>High Towns Est</h3>
+                            <p class="card-text">High Towns is the Trading and Contracting Company. We undertake all
+                                inquiries related to Automation Components. We would appreciate hearing your comments,
+                                and
+                                we will look forward to answering any questions you have about our products and our
+                                prices.
+                                We will be most happy to be of assistance. High Towns staff consist of highly skilled
+                                application engineers, sales engineers and administrative personnel who are committed to
+                                satisfy customers’ needs in the field of Industrial automation, automating services and
+                                electronic components.</p>
+                            <br>
+
+                            <p class="card-text">Our Scope of Supply: <strong> Bussmann, Comet, Omron, Farnell ,Sick,
+                                    Telemechanique, ABB, Mitsubishi, Allen Bradley and other brand Components</strong>
+                            </p>
+
+                            <br>
+                            <br>
+                            <p class="card-text">Our services include supply of high quality industrial control and
+                                automation products,
+                                electronic components, handling equipment’s and providing solutions in the following
+                                fields:- </p>
+
+                            <ol>
+                                <li>SCADA and Control monitoring solutions (HMI and PLC).</li>
+                                <li>Industrial and building automation systems.</li>
+                                <li>AC/DC drives.</li>
+                                <li>Pumps and Ventilation controls for HVAC systems.</li>
+                                <li>Drives and controls (Drive and PLC) for Lifts, hoists and cranes.</li>
+                                <li>Sensors and Switchgears</li>
+                                <li>Supplying All inverters with programming and installation.</li>
+                            </ol>
+                        </div>
+                        <div class="col-4">
+                            <div class="jumbotron">
+                                <h5>Contact Us:</h5>
+                                <p class="">sales@hightowns.com <br>+966540984014</p>
+                                <hr class="my-4">
+                                <h5>Address:</h5>
+                                <p class="">Building # 8072, 2nd Floor, Office # 6, Almarwah Distt,
+                                    Prince Mutaib street, PO Box 23342, Jeddah 21351, Saudi Arabia</p>
+                            </div>
+                            <img class="rounded img-fluid d-block" src="/images/brands.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+    </body>
+
+</html>
